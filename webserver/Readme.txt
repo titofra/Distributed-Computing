2d096f28ab8a1f1ff3b6a61fce923ba501ed5a76
Titouan ABADIE <120715525+titofra@users.noreply.github.com>
2023-04-12 21:18:12 +0000

    Add files via upload

---
+++ webserver/Readme.txt
@@ -0,0 +1,3 @@
+### WEBSERVER ###
+
+Make sure to save webserver_content's files with utf-8 encoding.
--- webserver/webserver.js
+++ webserver/webserver.js
@@ -1,1 +1,41 @@
+/*
+    @brief Basic webserver
+    @author Titouan ABADIE - http://github.com/titofra - 04/23
+*/
 
+const port = 1818;
+const root_path = './webserver_content';
+
+const fs = require('fs');
+const http = require('http');
+
+var server = http.createServer(function (req, res) {   //create web server
+    if (req.url == '/') {
+        fs.readFile(root_path + '/index', {encoding: 'utf-8'}, (err, data) => {
+            
+            if (err) {
+                console.log ('Invalid Request: ' + err);
+                res.end ('Invalid Request: ' + err);
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
+                res.write(data);
+                res.end();
+            }
+        });
+    } else {
+        fs.readFile(root_path + req.url, {encoding: 'utf-8'}, (err, data) => {
+            
+            if (err) {
+                console.log ('Invalid Request: ' + err);
+                res.end ('Invalid Request: ' + err);
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'}); 
+                res.write(data);
+                res.end();
+            }
+        });
+    }
+});
+
+server.listen(port);
+console.log('WebServer running port ' + port + ' ...')
